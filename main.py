89d54b4c4896001a1a09c36cbab6f19a4253d668
Anton Khorev <tony29@yandex.ru>
2013-10-24 13:13:20 +0400

    csvs for both department tables

--- main.py
+++ main.py
@@ -5,67 +5,86 @@ import re
 import csv
 from openpyxl import load_workbook
 
-wb=load_workbook('fincom/pr03-2014-16.xlsx')
-ws=wb.get_active_sheet()
-maxRow=ws.get_highest_row()
+def convertXslxToCsv(inputFilename,outputFilename,years):
+	wb=load_workbook(inputFilename)
+	ws=wb.get_active_sheet()
+	maxRow=ws.get_highest_row()
 
-for r in range(maxRow):
-	c=ws.cell(row=r,column=0)
-	if c.value==1:
-		break
-else:
-	raise Exception('table start not found')
-
-rows=[{}] # reserve first row for total
-departmentNameRe=re.compile(r'(?P<departmentName>.*?)\s*\((?P<departmentCode>...)\)')
-
-for r in range(r,maxRow):
-	number,name,sectionCode,categoryCode,typeCode,amount=(ws.cell(row=r,column=c).value for c in range(6))
-	sectionCode=sectionCode[:2]+sectionCode[-2:]
-	typeCode=str(typeCode)
-	if type(amount) is int:
-		amount=str(amount)+'.0'
+	for nRow in range(maxRow):
+		if ws.cell(row=nRow,column=0).value==1:
+			break
 	else:
-		amount=str(amount)
-	amount=decimal.Decimal(amount)
-	row={'year':2014}
-	if not number:
-		row['yearAmount']=amount
-		rows[0]=row
-		break
-	elif not sectionCode:
-		m=departmentNameRe.match(name)
-		row['departmentName']=departmentName=m.group('departmentName')
-		row['departmentCode']=departmentCode=m.group('departmentCode')
-		row['yearDepartmentAmount']=amount
-	elif not typeCode:
-		row['departmentName']=departmentName
-		row['departmentCode']=departmentCode
-		row['sectionCode']=sectionCode
-		row['categoryName']=categoryName=name
-		row['categoryCode']=categoryCode
-		row['yearDepartmentCategoryAmount']=amount
-	else:
-		row['departmentName']=departmentName
-		row['departmentCode']=departmentCode
-		row['sectionCode']=sectionCode
-		row['categoryName']=categoryName
-		row['categoryCode']=categoryCode
-		row['typeName']=typeName=name
-		row['typeCode']=typeCode
-		row['yearDepartmentCategoryTypeAmount']=amount
-	rows.append(row)
-else:
-	raise Exception('no total line found')
+		raise Exception('table start not found')
 
-writer=csv.writer(open('tables/pr03-2014-16.csv','w',newline='',encoding='utf8'),quoting=csv.QUOTE_NONNUMERIC)
-cols=[
-	'year',
-	'departmentName','departmentCode',
-	'sectionCode','categoryName','categoryCode',
-	'typeName','typeCode',
-	'yearAmount','yearDepartmentAmount','yearDepartmentCategoryAmount','yearDepartmentCategoryTypeAmount',
-]
-writer.writerow(cols)
-for row in rows:
-	writer.writerow([row.get(col) for col in cols])
+	rows=[{}]*len(years) # reserve first rows for totals
+	departmentNameRe=re.compile(r'(?P<departmentName>.*?)\s*\((?P<departmentCode>...)\)')
+
+	for nRow in range(nRow,maxRow):
+		def makeDecimalAmount(amount):
+			if type(amount) is int:
+				amount=str(amount)+'.0'
+			else:
+				amount=str(amount)
+			return decimal.Decimal(amount)
+		number,name,sectionCode,categoryCode,typeCode=(ws.cell(row=nRow,column=c).value for c in range(5))
+		amounts=[makeDecimalAmount(ws.cell(row=nRow,column=c).value) for c in range(5,5+len(years))]
+		sectionCode=sectionCode[:2]+sectionCode[-2:]
+		typeCode=str(typeCode)
+		amountCol=None
+		row={}
+		addRow=rows.append
+		if not number:
+			def makeAddRow():
+				nRow=0
+				def addRow(row):
+					nonlocal nRow,rows
+					rows[nRow]=row
+					nRow+=1
+				return addRow
+			addRow=makeAddRow()
+			amountCol='yearAmount'
+		elif not sectionCode:
+			m=departmentNameRe.match(name)
+			row['departmentName']=departmentName=m.group('departmentName')
+			row['departmentCode']=departmentCode=m.group('departmentCode')
+			amountCol='yearDepartmentAmount'
+		elif not typeCode:
+			row['departmentName']=departmentName
+			row['departmentCode']=departmentCode
+			row['sectionCode']=sectionCode
+			row['categoryName']=categoryName=name
+			row['categoryCode']=categoryCode
+			amountCol='yearDepartmentCategoryAmount'
+		else:
+			row['departmentName']=departmentName
+			row['departmentCode']=departmentCode
+			row['sectionCode']=sectionCode
+			row['categoryName']=categoryName
+			row['categoryCode']=categoryCode
+			row['typeName']=typeName=name
+			row['typeCode']=typeCode
+			amountCol='yearDepartmentCategoryTypeAmount'
+		for year,amount in zip(years,amounts):
+			r=row.copy()
+			r['year']=year
+			r[amountCol]=amount
+			addRow(r)
+		if not number:
+			break
+	else:
+		raise Exception('no total line found')
+
+	writer=csv.writer(open(outputFilename,'w',newline='',encoding='utf8'),quoting=csv.QUOTE_NONNUMERIC)
+	cols=[
+		'year',
+		'departmentName','departmentCode',
+		'sectionCode','categoryName','categoryCode',
+		'typeName','typeCode',
+		'yearAmount','yearDepartmentAmount','yearDepartmentCategoryAmount','yearDepartmentCategoryTypeAmount',
+	]
+	writer.writerow(cols)
+	for row in rows:
+		writer.writerow([row.get(col) for col in cols])
+
+convertXslxToCsv('fincom/pr03-2014-16.xlsx','tables/pr03-2014-16.csv',[2014])
+convertXslxToCsv('fincom/pr04-2014-16.xlsx','tables/pr04-2014-16.csv',[2015,2016])
