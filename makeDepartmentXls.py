dd9efdc9c65a263c65dd54d3c2e6f869e69bad29
Anton Khorev <tony29@yandex.ru>
2013-10-25 16:23:35 +0400

    write xls

--- makeDepartmentXls.py
+++ makeDepartmentXls.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sqlite3
+import xlwt3 as xlwt
 
 levelColLists=[
 	['departmentCode'],
@@ -55,5 +56,31 @@ with sqlite3.connect(':memory:') as conn:
 				newInsides=True
 		outRow[-len(years)+years.index(row['year'])]=row['amount']
 
-for outRow in outRows:
-	print(outRow)
+# write xls
+wb=xlwt.Workbook()
+ws=wb.add_sheet('expenditures')
+styleStandard=xlwt.easyxf('')
+styleHeader=xlwt.easyxf('font: bold on; align: wrap on')
+styleThinHeader=xlwt.easyxf('font: bold on, height 180; align: wrap on')
+styleVeryThinHeader=xlwt.easyxf('font: height 140; align: wrap on')
+styleAmount=xlwt.easyxf(num_format_str='#,##0.0')
+nHeaderRows=1
+ws.set_panes_frozen(True)
+ws.set_horz_split_pos(nHeaderRows)
+ws.row(nHeaderRows-1).height=1200
+columns=[
+	{'text':'Наименование',		'width':100,	'headerStyle':styleHeader,		'cellStyle':styleStandard},
+	{'text':'Код ведомства',	'width':4,	'headerStyle':styleVeryThinHeader,	'cellStyle':styleStandard},
+	{'text':'Код раздела',		'width':5,	'headerStyle':styleThinHeader,		'cellStyle':styleStandard},
+	{'text':'Код целевой статьи',	'width':8,	'headerStyle':styleThinHeader,		'cellStyle':styleStandard},
+	{'text':'Код вида расходов',	'width':4,	'headerStyle':styleVeryThinHeader,	'cellStyle':styleStandard},
+]+[{'text':'Сумма на '+str(year)+' г.',	'width':15,	'headerStyle':styleHeader,		'cellStyle':styleAmount} for year in years]
+for nCol,col in enumerate(columns):
+	ws.col(nCol).width=256*col['width']
+	ws.write(nHeaderRows-1,nCol,col['text'],col['headerStyle'])
+for nRow,row in enumerate(outRows):
+	for nCol,(cell,col) in enumerate(zip(row,columns)):
+		if cell is None:
+			continue
+		ws.write(nHeaderRows+nRow,nCol,cell,col['cellStyle'])
+wb.save('out/pr03,04-2014-16.xls')
