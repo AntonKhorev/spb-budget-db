07cdcf49cbe0c519bfb8ca0804b0d69e6eeea0ce
Anton Khorev <tony29@yandex.ru>
2013-11-20 20:51:47 +0400

    detect paragraphs

--- odf.py
+++ odf.py
@@ -1,9 +1,19 @@
+#!/usr/bin/env python3
+
+import re
 import ezodf
+
+paragraphRe=re.compile(r'Пункт N (?P<paragraphNumber>\d+(?:\.\d+)*)')
+
 doc=ezodf.opendoc('assembly/3765.odt')
 for obj in doc.body:
 	if type(obj) is ezodf.text.Paragraph:
-		print('text paragraph {',obj.plaintext(),'}')
+		print('text paragraph {')
+		for line in obj.plaintext().splitlines():
+			m=paragraphRe.match(line)
+			if m:
+				print('=== paragraph',m.group('paragraphNumber'),'===')
+			print('text line:',line)
+		print('}')
 	elif type(obj) is ezodf.table.Table:
 		print('table',obj.nrows(),'x',obj.ncols())
-	else:
-		print('something else',obj)
