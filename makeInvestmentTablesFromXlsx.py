9fb302c2221fade38684271b07a67a8f4ca90171
Anton Khorev <tony29@yandex.ru>
2013-12-30 15:39:53 +0400

    investments csv

--- makeInvestmentTablesFromXlsx.py
+++ makeInvestmentTablesFromXlsx.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import re
+import csv
 from openpyxl import load_workbook
 
 def makeInvestmentTableReaderFromXlsxFile(inputFilename):
@@ -20,5 +22,84 @@ def makeInvestmentTableReaderFromXlsxFile(inputFilename):
 			nRow+=1
 	return reader
 
-for row in makeInvestmentTableReaderFromXlsxFile('fincom/2014.0.z/pr24_bd2014-16.xlsx')():
-	print(row)
+class InvestmentTableWriter:
+	def __init__(self,tableReader,years):
+		self.rows=[]
+		self.levelCols=[
+			['grandTotal'],
+			['departmentName','departmentTotal'],
+			['branchName','branchTotal'],
+			['recipientName','recipientTotal'],
+			['projectName','districtNames','projectFirstYear','projectLastYear','projectDurationTotal','amount'],
+		]
+		yearRangeRe=re.compile(r'(\d{4})\s*-\s*(\d{4})')
+		rows=[{'year':year} for year in years]
+		def setLevel(toLevel):
+			for level,cols in enumerate(self.levelCols):
+				if toLevel<level:
+					for col in cols:
+						for row in rows:
+							row.pop(col,None)
+		def setColValue(k,v):
+			for row in rows:
+				row[k]=v
+		for name,districtNames,yearRange,projectDurationTotal,*amounts in tableReader():
+			def setColAmounts(k):
+				for row,v in zip(rows,amounts):
+					row[k]=v
+			def update():
+				prefix='ВСЕГО:'
+				if name.startswith(prefix):
+					setLevel(0)
+					setColAmounts('grandTotal')
+					return
+				prefix='ЗАКАЗЧИК: '
+				if name.startswith(prefix):
+					departmentName=name[len(prefix):]
+					setLevel(1)
+					setColValue('departmentName',departmentName)
+					setColAmounts('departmentTotal')
+					return
+				prefix='ОТРАСЛЬ: '
+				if name.startswith(prefix):
+					branchName=name[len(prefix):]
+					setLevel(2)
+					setColValue('branchName',branchName)
+					setColAmounts('branchTotal')
+					return
+				prefix='БЮДЖЕТНЫЕ ИНВЕСТИЦИИ '
+				if name.startswith(prefix):
+					recipientName=name[len(prefix):] # TODO fix grammatic cases
+					setLevel(3)
+					setColValue('recipientName',recipientName)
+					setColAmounts('recipientTotal')
+					return
+				setLevel(4)
+				setColValue('projectName',name)
+				setColValue('districtNames',districtNames)
+				m=yearRangeRe.match(str(yearRange))
+				if m:
+					projectFirstYear=int(m.group(1))
+					projectLastYear=int(m.group(2))
+				else:
+					projectFirstYear=projectLastYear=None if yearRange is None else int(yearRange)
+				setColValue('projectFirstYear',projectFirstYear)
+				setColValue('projectLastYear',projectLastYear)
+				setColValue('projectDurationTotal',projectDurationTotal)
+				setColAmounts('amount')
+			update()
+			for row in rows:
+				self.rows.append(row.copy())
+	def write(self,outputFilename):
+		cols=['year']+[col for cols in self.levelCols for col in cols]
+		with open(outputFilename,'w',newline='',encoding='utf8') as file:
+			writer=csv.writer(file,quoting=csv.QUOTE_NONNUMERIC)
+			writer.writerow(cols)
+			for row in self.rows:
+				writer.writerow([row.get(col) for col in cols])
+
+documentNumber=3850
+InvestmentTableWriter(
+	makeInvestmentTableReaderFromXlsxFile('fincom/2014.0.z/pr24_bd2014-16.xlsx'),
+	[2014,2015,2016]
+).write('tables/2014.0.z.'+str(documentNumber)+'.investment.csv')
