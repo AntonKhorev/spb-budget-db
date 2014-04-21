427ad28795c0fd731ca521e2cd82ea9317da0c51
Anton Khorev <tony29@yandex.ru>
2014-04-21 08:32:02 +0400

    make tables from BudgetSpb's csvs

---
+++ makeTablesFromCsv.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import tableWriters
+import csv
+
+def makeTableReaderFromCsvFile(inputFilename,nAmountCols):
+	def makeAmount(amount):
+		return amount.replace(',','.')
+	def makeRow(row):
+		return row[:5]+[makeAmount(a) for a in row[5:5+nAmountCols]]
+	with open(inputFilename,encoding='utf8',newline='') as inputFile:
+		reader=csv.reader(inputFile)
+		next(reader)
+		totalRow=next(reader)
+		for row in reader:
+			yield makeRow(row)
+		yield makeRow(totalRow)
+
+# project of 1st correction
+documentNumber=4597
+tableWriters.DepartmentTableWriter(
+	makeTableReaderFromCsvFile('fincom/2014.1.p/2014.1.p-2014(3).csv',1),
+	[2014]
+).write('tables/2014.1.p.'+str(documentNumber)+'.3.department.csv')
+tableWriters.DepartmentTableWriter(
+	makeTableReaderFromCsvFile('fincom/2014.1.p/2014.1.p-2015,2016(3).csv',2),
+	[2015,2016]
+).write('tables/2014.1.p.'+str(documentNumber)+'.4.department.csv')
