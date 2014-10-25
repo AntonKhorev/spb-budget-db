2d91dbc9f49ed5749c588e63a5ec7c160f7fc538
Anton Khorev <tony29@yandex.ru>
2014-10-25 11:21:03 +0400

    category name - code set changes over years

--- 3-db/dataSets.py
+++ 3-db/dataSets.py
@@ -137,7 +137,6 @@ class InterYearSet:
 
 class CategoryTrackingSet:
 	def __init__(self,yearSets):
-		yearSet=yearSets[0]
 		print('== experimental log ==')
 		categoryNameCodeAmounts=collections.defaultdict(lambda: collections.defaultdict(decimal.Decimal)) # categoryName -> categoryCode -> amount
 		def getCategoryCodeset():
@@ -147,28 +146,33 @@ class CategoryTrackingSet:
 				c[name]=set(codeAmounts.keys())
 			return c
 		categoryCodeset2=getCategoryCodeset()
-		documentEdits=collections.OrderedDict()
-		for edit in yearSet.edits:
-			if edit['documentNumber'] not in documentEdits:
-				documentEdits[edit['documentNumber']]=[]
-			documentEdits[edit['documentNumber']].append(edit['editNumber'])
-		# for editNumber in range(1,len(yearSet.edits)+1):
-		for documentNumber,editNumbers in documentEdits.items():
-			categoryCodeset1=categoryCodeset2
-			for key,editAmounts in yearSet.items.items.items():
-				for editNumber in editNumbers:
-					if editNumber not in editAmounts:
-						continue
-					fiscalYear,departmentCode,sectionCode,categoryCode,typeCode=key
-					categoryName=yearSet.categories.names[categoryCode]
-					categoryNameCodeAmounts[categoryName][categoryCode]+=editAmounts[editNumber]
-					if categoryNameCodeAmounts[categoryName][categoryCode]==0:
-						del categoryNameCodeAmounts[categoryName][categoryCode]
-			categoryCodeset2=getCategoryCodeset()
-			for categoryName in categoryCodeset2:
-				cs1=categoryCodeset1[categoryName]
-				cs2=categoryCodeset2[categoryName]
-				if len(cs1)==0 and len(cs2)==1 or len(cs1)==1 and len(cs2)==0:
-					continue # not interesting
-				if cs1!=cs2:
-					print('doc',documentNumber,'had',cs1,'got',cs2,'cat',categoryName)
+		for yearSet in yearSets:
+			# reset accumulated amounts
+			categoryNameCodeAmounts.clear()
+			# group edits by documents
+			documentEdits=collections.OrderedDict()
+			for edit in yearSet.edits:
+				if edit['documentNumber'] not in documentEdits:
+					documentEdits[edit['documentNumber']]=[]
+				documentEdits[edit['documentNumber']].append(edit['editNumber'])
+			# walk over documents
+			# for editNumber in range(1,len(yearSet.edits)+1):
+			for documentNumber,editNumbers in documentEdits.items():
+				categoryCodeset1=categoryCodeset2
+				for key,editAmounts in yearSet.items.items.items():
+					for editNumber in editNumbers:
+						if editNumber not in editAmounts:
+							continue
+						fiscalYear,departmentCode,sectionCode,categoryCode,typeCode=key
+						categoryName=yearSet.categories.names[categoryCode]
+						categoryNameCodeAmounts[categoryName][categoryCode]+=editAmounts[editNumber]
+						if categoryNameCodeAmounts[categoryName][categoryCode]==0:
+							del categoryNameCodeAmounts[categoryName][categoryCode]
+				categoryCodeset2=getCategoryCodeset()
+				for categoryName in categoryCodeset2:
+					cs1=categoryCodeset1[categoryName]
+					cs2=categoryCodeset2[categoryName]
+					if len(cs1)==0 and len(cs2)==1 or len(cs1)==1 and len(cs2)==0:
+						continue # not interesting
+					if cs1!=cs2:
+						print('doc',documentNumber,'had',cs1,'got',cs2,'cat',categoryName)
